74ed383b2bce338ac4d3d4c1b9c6d23adbbf26ab
Ahmed Ayari <143943682+Sphynx6@users.noreply.github.com>
2024-02-08 15:51:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ let EXBD = document.getElementById('noteexBD');
 let DSreseau = document.getElementById('notedsreseau');
 let EXreseau = document.getElementById('noteexreseau');
 let DSJava = document.getElementById('notedsJava');
-let ExJava = document.getElementById('noteexJava');
+let EXJava = document.getElementById('noteexJava');
 let DSSI = document.getElementById('notedsSI');
 let EXSI = document.getElementById('noteexSI');
 let DSMath = document.getElementById('notedsMath');
@@ -63,34 +63,41 @@ let coeffOr = 0.2;
 let coeffDs1 = 0.4;
 let coeffDs2 = 0.4;
 
-//Calcul des moyennes
-    //Calcul des moyennes des modules RM
-function calculerM(ds1, ex1, ds2, ex2, coef1, coef2){
-    return MM = ( ((ds1 * coeffDS + ex1 * coeffEX)*coef1) + ((ds2 * coeffDS + ex2 * coeffEX)*coef2 ))/ (coef1+coef2);
-}
-    //Calcul des moyennes des modules CC
-function calculerMCC(or1, ds1, ds2, or2, ds3, ds4){
-    return MMCC = ((or1 * coeffOr)+(ds1 * coeffDs1)+(ds2 * coeffDs2) + (or2 * coeffOr)+(ds3 * coeffDs1)+(ds4 * coeffDs2))/2
+//credit
+    //credit matiere
+let creditProba = 4
+let creditGo = 2
+let creditTLA = 2
+let creditBD = 3
+let creditReseau = 2
+let creditJava = 4
+let creditSI = 3
+let creditMath = 3
+let creditPhy = 3
+let creditEng = 2
+let creditGE = 2
+
+let creditMod1 = creditProba
+let creditMod2 = creditGo + creditTLA
+let creditMod3 = creditBD + creditReseau
+let creditMod4 = creditJava + creditSI
+let creditMod5 = creditMath + creditPhy
+let creditMod6 = creditEng + creditGE
+    //credit total
+
+let CreditTotal = 0
+
+//fonctions
+function VerifValeur(x, y){
+    if(x>20 || x<0){
+        alert("Note de " + y + " entre 0 et 20")
+        Clear();
+    }
 }
 
-function calculer(){
-    Clear();
-    let m1 = calculerM(DSProba.value, EXProba.value, 0, 0, coeffProba, 0);
-    let m2 = calculerM(DSGO.value, EXGO.value, DSTLA.value, EXTLA.value, coeffGO, coeffTLA);
-    let m3 = calculerM(DSBD.value, EXBD.value, DSreseau.value, EXreseau.value, coeffBD, coeffreseau);
-    let m4 = calculerM(DSJava.value, ExJava.value, DSSI.value, EXSI.value, coeffJava, coeffSI);
-    let m5 = calculerM(DSMath.value, EXMath.value, DSPhy.value, EXPhy.value, coeffMath, coeffPhy);
-    let m6 = calculerMCC(OrEng.value, DSEng1.value, DSEng2.value, OrGE.value, DSGE1.value, DSGE2.value);
-
-    module1.innerHTML += m1.toFixed(2)
-    module2.innerHTML += m2.toFixed(2)
-    module3.innerHTML += m3.toFixed(2)
-    module4.innerHTML += m4.toFixed(2)
-    module5.innerHTML += m5.toFixed(2)
-    module6.innerHTML += m6.toFixed(2)
-    moyenne.innerHTML += ((m1*coeffMod1 + m2*coeffMod2 + m3*coeffMod3 + m4*coeffMod4 + m5*coeffMod5 + m6*coeffMod6)/ coeffTotal).toFixed(2)
+function calculerM(matiere1, matiere2, coef1, coef2){
+    return  ((matiere1*coef1) + (matiere2*coef2 ))/ (coef1+coef2);
 }
-
 function Clear(){
     module1.innerHTML = "Moyenne Module: "
     module2.innerHTML = "Moyenne Module: "
@@ -103,3 +110,96 @@ function Clear(){
     credit.innerHTML = "Credit: ";
 }
 
+function verifCredit(mo, cmo, m1, m2, cm1, cm2){
+    if(mo>=10){
+        CreditTotal += cmo 
+    }
+    else{
+        if(m1>=10){
+            CreditTotal += cm1
+        }
+        else if(m2>=10){
+            CreditTotal += cm2
+        }
+    }
+}
+
+function CalculeMoyenne(ds , ex){
+    return (ds * coeffDS + ex * coeffEX)
+}
+
+function CalculeMoyenneCC(orale, ds1, ds2){
+    return (orale * coeffOr + ds1 * coeffDs1 + ds2 * coeffDs2)
+}
+
+
+//main
+function calculer(){
+    Clear();
+    CreditTotal = 0
+    //verification des notes saisi
+    VerifValeur(DSProba.value, "Proba")
+    VerifValeur(EXProba.value, "Proba")
+    VerifValeur(DSGO.value, "GO")
+    VerifValeur(EXGO.value, "GO")
+    VerifValeur(DSTLA.value, "TLA")
+    VerifValeur(EXTLA.value, "TLA")
+    VerifValeur(DSBD.value, "BD")
+    VerifValeur(EXBD.value, "BD")
+    VerifValeur(DSreseau.value, "Reseau")
+    VerifValeur(EXreseau.value, "Reseau")
+    VerifValeur(DSJava.value, "Java")
+    VerifValeur(EXJava.value, "Java")
+    VerifValeur(DSSI.value, "SI")
+    VerifValeur(EXSI.value, "SI")
+    VerifValeur(DSMath.value, "Math")
+    VerifValeur(EXMath.value, "Math")
+    VerifValeur(DSPhy.value, "Phy")
+    VerifValeur(EXPhy.value, "Phy")
+    VerifValeur(OrEng.value, "Anglais")
+    VerifValeur(DSEng1.value, "Anglais")
+    VerifValeur(DSEng2.value, "Anglais")
+    VerifValeur(OrGE.value, "GE")
+    VerifValeur(DSGE1.value, "GE")
+    VerifValeur(DSGE2.value, "GE")
+
+    //Calcul des moyennes
+    let proba = CalculeMoyenne(DSProba.value, EXProba.value)
+    let go = CalculeMoyenne(DSGO.value, EXGO.value)
+    let tla = CalculeMoyenne(DSTLA.value, EXTLA.value)
+    let bd = CalculeMoyenne(DSBD.value, EXBD.value)
+    let reseau = CalculeMoyenne(DSreseau.value, EXreseau.value)
+    let java = CalculeMoyenne(DSJava.value, EXJava.value)
+    let si = CalculeMoyenne(DSSI.value, EXSI.value)
+    let math = CalculeMoyenne(DSMath.value, EXMath.value)
+    let phy = CalculeMoyenne(DSPhy.value, EXPhy.value)
+    let eng = CalculeMoyenneCC(OrEng.value, DSEng1.value, DSEng2.value)
+    let ge = CalculeMoyenneCC(OrGE.value, DSGE1.value, DSGE2.value)
+
+    //calcul des modules
+    let m1 = calculerM(proba, 0, coeffProba, 0);
+    let m2 = calculerM(go, tla, coeffGO, coeffTLA);
+    let m3 = calculerM(bd, reseau, coeffBD, coeffreseau);
+    let m4 = calculerM(java, si, coeffJava, coeffSI);
+    let m5 = calculerM(math, phy, coeffMath, coeffPhy);
+    let m6 = calculerM(eng, ge, coeffEng, coeffGE);
+
+    //credit
+    verifCredit(m1, creditMod1, proba, 0, creditProba, 0)
+    verifCredit(m2, creditMod2, go, tla, creditGo, creditTLA)
+    verifCredit(m3, creditMod3, bd, reseau, creditBD, creditReseau)
+    verifCredit(m4, creditMod4, java, si, creditJava, creditSI)
+    verifCredit(m5, creditMod5, math, phy, creditMath, creditPhy)
+    verifCredit(m6, creditMod6, eng, ge, creditEng, creditGE)
+    
+    //affichage
+    module1.innerHTML += m1.toFixed(2)
+    module2.innerHTML += m2.toFixed(2)
+    module3.innerHTML += m3.toFixed(2)
+    module4.innerHTML += m4.toFixed(2)
+    module5.innerHTML += m5.toFixed(2)
+    module6.innerHTML += m6.toFixed(2)
+    moyenne.innerHTML += (((m1*coeffMod1) + (m2*coeffMod2) + (m3*coeffMod3) + (m4*coeffMod4) + (m5*coeffMod5) + (m6*coeffMod6))/ coeffTotal).toFixed(2)
+    credit.innerHTML += CreditTotal
+
+}
